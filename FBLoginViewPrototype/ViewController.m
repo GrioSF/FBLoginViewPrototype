85f1f0a0a4b246edb6cf1282744c85a0cd7edf92
Giuseppe Macri <gmacri@grio.com>
2013-01-24 23:09:39 -0800

    First commit

--- FBLoginViewPrototype/ViewController.m
+++ FBLoginViewPrototype/ViewController.m
@@ -7,23 +7,55 @@
 //
 
 #import "ViewController.h"
+#import <FacebookSDK/FacebookSDK.h>
 
-@interface ViewController ()
+@interface ViewController () <FBLoginViewDelegate>
+@property (weak, nonatomic) IBOutlet FBLoginView *fbLoginView;
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet FBProfilePictureView *profilePic;
+
 
 @end
 
-@implementation ViewController
+@implementation ViewController 
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Create Login View so that the app will be granted "status_update" permission.
+    FBLoginView *loginview = [[FBLoginView alloc] init];
+    
+    loginview.frame = CGRectOffset(loginview.frame, 5, 5);
+    loginview.delegate = self;
+    
+    [self.view addSubview:loginview];
+    
+    [loginview sizeToFit];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - FBLoginViewDelegate
+
+- (void)loginViewShowingLoggedInUser:(FBLoginView *)loginView {
+    // first get the buttons set for login mode
+}
+
+- (void)loginViewFetchedUserInfo:(FBLoginView *)loginView
+                            user:(id<FBGraphUser>)user {
+    self.usernameLabel.text = user.first_name;
+    self.profilePic.profileID = user.id;
+}
+
+- (void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView {
+    self.profilePic.profileID = nil;
+    self.usernameLabel.text = nil;
 }
 
 @end
