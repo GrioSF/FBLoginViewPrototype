e92d90619681301e50ff71de9628c97e7ff9503d
Giuseppe Macri <gmacri@grio.com>
2013-01-24 22:04:47 -0800

    Initial Commit

---
+++ FBLoginViewPrototype/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FBLoginViewPrototype
+//
+//  Created by Giuseppe Macri on 1/24/13.
+//  Copyright (c) 2013 grio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FBLoginViewPrototype/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FBLoginViewPrototype
+//
+//  Created by Giuseppe Macri on 1/24/13.
+//  Copyright (c) 2013 grio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
